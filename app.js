ab4a7ee97eae34e3b105935daa371e536f4f1d68
LuqmanKhokhar <Abdrehman2400@gmail.com>
2024-04-20 10:15:58 +0500

    completed code

--- app.js
+++ app.js
@@ -41,6 +41,92 @@ const questions = [
 ];
 
 
-const questionElement = document.getElementsByClassName(".question");
-const answerElement = document.getElementsByClassName(".answers");
-const nextbtn = document.getElementsByClassName(".next-btn");
+
+const questionElement = document.querySelector(".question");
+const answerButton = document.querySelector(".answers");
+const nextbtn = document.querySelector(".next-btn");
+
+
+let currQuesIdx = 0;
+let score = 0;
+
+function startQuiz() {
+  currQuesIdx = 0;
+  score = 0;
+  nextbtn.innerHTML = "Next";
+  showQues();
+
+} 
+
+
+function showQues() {
+
+  resetState();
+  let currQues = questions[currQuesIdx];
+  let quesNo = currQuesIdx + 1;
+  questionElement.innerHTML = quesNo + ". " + currQues.question; 
+
+  currQues.answer.forEach(answer => {
+    const btn = document.createElement("button");
+    btn.innerHTML = answer.text;
+    btn.classList.add("btn");
+    answerButton.appendChild(btn);  
+    if(answer.correct){
+      btn.dataset.correct = answer.correct;
+    }
+    btn.addEventListener("click", selectAnswer);
+  });
+};
+
+const selectAnswer =(e) => {
+  const selectedBtn = e.target;
+  const isCorrect = selectedBtn.dataset.correct === "true";
+  if(isCorrect){
+    selectedBtn.classList.add("correct");
+    score++;
+  }else{
+    selectedBtn.classList.add("incorrect");
+  }
+  Array.from(answerButton.children).forEach(btn => {
+    if(btn.dataset.correct === "true"){
+      btn.classList.add("correct");
+    }
+    btn.disabled = true;
+  }) ;
+  nextbtn.style.display = "block";
+};
+
+const resetState = () => {
+  nextbtn.style.display = "none";
+  while(answerButton.firstChild) {
+    answerButton.removeChild(answerButton.firstChild);
+  }
+};
+
+
+nextbtn.addEventListener("click", ()=> {
+  if(currQuesIdx < questions.length){
+    handleNextBtn();
+  }else{
+    startQuiz();
+  }
+})
+
+
+const handleNextBtn = () => {
+  currQuesIdx++;
+  if(currQuesIdx < questions.length){
+    showQues();
+  }else{
+    showScore();
+  }
+};
+
+const showScore = () => {
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}`
+    nextbtn.innerHTML = "Play Again";
+    nextbtn.style.display = "block";
+}
+
+startQuiz();
